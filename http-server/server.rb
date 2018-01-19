b36e7e727e2544c4392ec663b5f244b5aabb55d6
Edward McCarthy <efmccarthy26@gmail.com>
2018-01-18 19:14:14 -0700

    Added helper functions to parse Request-Line and generate path to file on server

--- http-server/server.rb
+++ http-server/server.rb
@@ -1,13 +1,33 @@
 require "socket"
+require "uri"
+
+WEB_ROOT = "./public"
+
+CONTENT_TYPE_MAPPING = {
+  "html" => "text/html",
+  "txt"  => "text/plain",
+  "png" => "image/png",
+  "jpg" => "image/jpeg"
+}
+
+DEFAULT_CONTENT_TYPE = "application/octet-stream"
+
+def content_type(path)
+  ext = File.extname(path).split(".").last
+  CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+end
+
+def requested_file(request_line)
+
+end
 
 server = TCPServer.new("localhost", 2345)
 
 loop do
   socket = server.accept
-
   request = socket.gets
 
-  STDERR.puts request
+  STDERR.puts request_line
 
   response = "We're Live!\n"
 
